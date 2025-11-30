fc963a87e75d7b292c3d5d8d7e36a1437d8dd691
MrRazor22 <senthilvenkat22@gmail.com>
2025-11-30 09:15:15 +0530

    test project created

---
+++ AgentCore.Tests/Runtime/AgentTests.cs
@@ -0,0 +1,16 @@
+﻿using AgentCore.Chat;
+using AgentCore.Runtime;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgentCore.Tests.Runtime
+{
+    internal class AgentTests
+    {
+
+    }
+}
