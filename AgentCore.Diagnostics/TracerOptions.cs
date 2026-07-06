f57673ddf0a5544177d1dba2719481d3560fd25a
MrRazor22 <senthilvenkat22@gmail.com>
2026-07-06 22:55:52 +0530

    Deleted diagnostics projects

--- AgentCore.Diagnostics/TracerOptions.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace AgentCore.Diagnostics;
-
-public class TracerOptions
-{
-    public IList<ITraceExporter> Exporters { get; } = new List<ITraceExporter>();
-    public IList<ITraceObserver> Observers { get; } = new List<ITraceObserver>();
-}
